e634606a3c52ce3f8948774a904c7f35907c9ea0
reimiii <imiia75775@gmail.com>
2023-06-18 13:44:25 +0700

    where_clause

---
+++ where_clause.sql
@@ -0,0 +1,15 @@
+select *
+from products
+where quantity = 100;
+
+select *
+from products
+where price = 15000;
+
+select *
+from products
+where id = 'P0001';
+
+select *
+from products
+where name = 'mie ayam bakso';
