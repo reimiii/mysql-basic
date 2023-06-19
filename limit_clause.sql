7949c0321b569c784c9a974285e9699ffc3f9fae
reimiii <imiia75775@gmail.com>
2023-06-19 16:13:04 +0700

    limit_clause

---
+++ limit_clause.sql
@@ -0,0 +1,29 @@
+select *
+from products
+order by id
+limit 5;
+
+select *
+from products
+order by id
+limit 2;
+
+select *
+from products
+order by id
+limit 0, 5;
+
+select *
+from products
+order by id
+limit 5, 5;
+
+select *
+from products
+order by id
+limit 10, 5;
+
+select *
+from products
+order by id
+limit 15, 5;
